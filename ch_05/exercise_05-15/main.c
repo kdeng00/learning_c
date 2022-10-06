6fde7ef2b3c8220f14e3f49ac300b92a6686a953
kdeng00 <kundeng94@outlook.com>
2022-10-06 19:23:43 -0400

    Added exercise to work on

---
+++ ch_05/exercise_05-15/main.c
@@ -0,0 +1,16 @@
+/*
+ *
+ * Exercise 5-15. Add the option -f to fold upper and lower case together, so that case
+distinctions are not made during sorting; for example, a and A compare equal.
+ *
+ * Author: Kun Deng
+ */
+
+
+main(int argc, char **argv)
+{
+    auto i = 0;
+
+
+    return 0;
+}
